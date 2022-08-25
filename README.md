3a51df84050cb7d66b348e58692ce59e0d51b279
Muhammad Aaqil <aaqilniz@yahoo.com>
2022-08-25 19:14:15 +0500

    typescript version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lb4-route-script
+
 An initial script to create LoopBack 4 custom controllers/routes by running `npm run custom-controller --params`
+
+node index.js -q="todo|dsearchtree|get|ut|fza:string,hst:string,ht:string|SELECT distinct(UT), UTB FROM dsearchtree.datecode2 where FZA eq \${fza} and HST eq \${hst} and HT eq \${ht} order by UTB ASC;"
+
+node index.js -q="todo|dsearchtree|get|ut|fza:string,hst:string,ht:string|SELECT distinct(UT), UTB FROM dsearchtree.datecode2 where FZA eq \${fza} and HST eq \${hst} and HT eq \${ht} order by UTB ASC;"
--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function getQuery() {
     })
 
     async ${queryMethod}(@param.filter(${modelName}) filter?: Filter<Todo>): Promise<${modelName}[]> {
-      return this.${parts[0]}Repository.execute('${sqlQuery}');
+      return this.${parts[0]}.execute('${sqlQuery}');
     }
   `;
   const controller = `
@@ -74,3 +74,90 @@ function getQuery() {
 function capitalize(s) {
   return s[0].toUpperCase() + s.slice(1);
 }
+
+
+
+/*
+
+  interface InputParams {
+  [key: string]: unknown;
+}
+
+function capitalize(s: string) {
+  return s[0].toUpperCase() + s.slice(1);
+}
+
+function getQuery() {
+  let query = '';
+  const {argv} = process;
+  const args = argv.slice(2, argv.length);
+  if (!args.length) {
+    throw Error('Parameters are required.');
+  }
+  args.forEach(arg => {
+    const argument = arg.split('=');
+    let argumentFlag = '';
+    let sliceUpto = 0;
+    if (arg[0] === '-') sliceUpto = 1;
+    if (arg.slice(0, 2) === '--') sliceUpto = 2;
+    argumentFlag = argument[0].slice(sliceUpto, argument[0].length);
+    if (argumentFlag === 'q' || argumentFlag === 'query') {
+      query = argument.length > 1 ? argument[1] : '';
+    }
+  });
+  return query.replace(/eq/g, '=');
+}
+
+(() => {
+  const query = getQuery();
+  const parts = query.split('|');
+  const modelName = capitalize(parts[0]);
+  const existingRepo = parts[1];
+  const httpMethod = parts[2];
+  const newMethod = parts[3];
+  const params = parts[4].split(',') || [];
+  const inputParams: InputParams = {};
+  let queryMethod = '';
+  switch (httpMethod) {
+    case 'get':
+      queryMethod = 'find';
+      break;
+    case 'post':
+      queryMethod = 'create';
+      break;
+  }
+  params.forEach((param: string) => {
+    const paramParts = param.split(':');
+    inputParams[paramParts[0]] = paramParts[1] as unknown as string;
+  });
+  const sqlQuery = parts[5];
+  console.log('Model Name: ', modelName);
+  console.log('Existing Repo: ', existingRepo);
+  console.log('New Method: ', newMethod);
+  console.log('HTTP Method: ', httpMethod);
+  console.log('Input Params: ', inputParams);
+  console.log('SQL query: ', sqlQuery);
+  const imports = ``;
+  const constructor = `constructor() {}`;
+  const customRoute = `${httpMethod}('/${newMethod}')
+    @response(200, {
+    description: '${modelName} custom model instance',
+    content: {'application/json': {schema: getModelSchemaRef(${modelName})}},
+    })
+
+    async ${queryMethod}(@param.filter(${modelName}) filter?: Filter<Todo>): Promise<${modelName}[]> {
+      return this.${parts[0]}.execute('${sqlQuery}');
+    }
+  `;
+  const controller = `
+  ${imports}
+  export class ${modelName}Controller {
+    ${constructor}
+
+    ${customRoute}
+  }`;
+  console.log(controller);
+})();
+
+
+*/
